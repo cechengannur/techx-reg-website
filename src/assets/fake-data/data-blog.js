d1fa5ea76a26447a67bc8aca2d1de64cf6f23529
alwin <alwinjohn231@gmail.com>
2022-12-19 02:04:47 +0530

    data+favicon+title updated

--- src/assets/fake-data/data-blog.js
+++ src/assets/fake-data/data-blog.js
@@ -1,56 +1,56 @@
-import img1 from '../images/post/imgpost11.jpg'
-import img2 from '../images/post/imgpost13.jpg'
-import img3 from '../images/post/imgpost12.jpg'
+import img1 from '../images/post/hack-league-reg.png'
+import img2 from '../images/post/Logo-Launch.png'
+import img3 from '../images/post/volley-ball.png'
 
 const dataBlog = [
     {
         id: 1,
         img : img1,
-        category: 'digitalart',
-        title: 'NFT Guide: Why They Matter For Music?',
-        time: '20 Jun 2022',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor.',
+        category: 'hackathon',
+        title: 'Hack-League, a 24-hour Hackathon',
+        time: '17 Dec 2022',
+        text: 'College of Engineering Chengannur is here with HACK-LEAGUE, a 24-hour hackathon',
     },
     {
         id: 2,
         img : img2,
-        category: 'nftartwork',
-        title: 'Max Live X Tacnocde Matter NFT Music ',
-        time: '20 Jun 2022',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor.',
+        category: 'logo-launch',
+        title: 'Official Logo Launch of IHRD Tharang\'23',
+        time: '14 Dec 2022',
+        text: 'A National Techno-Cultural-Entrepreneurial Fest Organized by IHRD',
     },
     {
         id: 3,
         img : img3,
-        category: 'digitalart',
-        title: 'NFT Guide: Why They Matter For Music?',
-        time: '20 Jun 2022',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor.',
+        category: 'sports',
+        title: 'Volleyball Tournament (Open to All Engr. Institutions)',
+        time: '13 Dec 2022',
+        text: 'The Volleyball tournament organized by the IHRD THARANG committee. Open to All Engr. Institutions',
     },
-    {
-        id: 4,
-        img : img1,
-        category: 'digitalart',
-        title: 'NFT Guide: Why They Matter For Music?',
-        time: '20 Jun 2022',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor.',
-    },
-    {
-        id: 5,
-        img : img2,
-        category: 'nftartwork',
-        title: 'Max Live X Tacnocde Matter NFT Music ',
-        time: '20 Jun 2022',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor.',
-    },
-    {
-        id: 6,
-        img : img3,
-        category: 'digitalart',
-        title: 'NFT Guide: Why They Matter For Music?',
-        time: '20 Jun 2022',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor.',
-    },
+    // {
+    //     id: 4,
+    //     img : img1,
+    //     category: 'digitalart',
+    //     title: 'NFT Guide: Why They Matter For Music?',
+    //     time: '20 Jun 2022',
+    //     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor.',
+    // },
+    // {
+    //     id: 5,
+    //     img : img2,
+    //     category: 'nftartwork',
+    //     title: 'Max Live X Tacnocde Matter NFT Music ',
+    //     time: '20 Jun 2022',
+    //     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor.',
+    // },
+    // {
+    //     id: 6,
+    //     img : img3,
+    //     category: 'digitalart',
+    //     title: 'NFT Guide: Why They Matter For Music?',
+    //     time: '20 Jun 2022',
+    //     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor.',
+    // },
 ]
 
 export default dataBlog;
