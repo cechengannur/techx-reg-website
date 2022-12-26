e27c150d81cacc0686353f5c55fbc5c48e5458f5
alwin <alwinjohn231@gmail.com>
2022-12-26 11:00:09 +0530

    added event details

--- src/assets/fake-data/dataProject.js
+++ src/assets/fake-data/dataProject.js
@@ -1,140 +1,154 @@
-import img1 from '../images/common/img1.jpg'
-import img2 from '../images/common/img2.jpg'
-import img3 from '../images/common/img3.jpg'
-import img4 from '../images/common/img4.jpg'
-import img5 from '../images/common/img5.jpg'
-import img6 from '../images/common/img6.jpg'
-import img7 from '../images/common/img7.jpg'
-import img9 from '../images/common/img9.jpg'
-import img10 from '../images/common/img10.jpg'
-import img11 from '../images/common/img11.jpg'
-import img12 from '../images/common/img12.jpg'
-import img13 from '../images/common/img13.jpg'
-import img14 from '../images/common/img14.jpg'
+import img1 from '../images/common/game-valorant.jpeg'
+import img2 from '../images/common/game-roadToGlory.jpeg'
+import img3 from '../images/common/game-compcod.jpeg'
+import { region } from 'caniuse-lite';
+// import img4 from '../images/common/img4.jpg'
+// import img5 from '../images/common/img5.jpg'
+// import img6 from '../images/common/img6.jpg'
+// import img7 from '../images/common/img7.jpg'
+// import img9 from '../images/common/img9.jpg'
+// import img10 from '../images/common/img10.jpg'
+// import img11 from '../images/common/img11.jpg'
+// import img12 from '../images/common/img12.jpg'
+// import img13 from '../images/common/img13.jpg'
+// import img14 from '../images/common/img14.jpg'
+import { Link } from 'react-router-dom';
 
 
 const dataProject = [
+    // {
+    //     id: 1,
+    //     title: 'RESURENZE',
+    //     img: img1,
+    //     category: '#game',
+    //     desc:"Excited about online tactical battle shooter games? Then brace for an exhilarating Valorant showdown - a team-based tactical PVP where your shooting skills, strategy, and team spirit are put to the test.\nSo, don your finest attire and arm yourself with cunning and wit, for the battle of wits is about to begin. Assemble your squad and get ready for a pulse-pounding showdown as IHRD Tharang presents the first edition of RESURENZE.\n🔴 Note : A team must have 5 members.",
+    //     link:"",
+    // },
     {
         id: 1,
-        title: 'CYBOX #01',
-        img: img1,
-    },
-    {
-        id: 2,
-        title: 'CYBOX #02',
+        title: 'Road to Glory',
         img: img2,
+        category: '#game',
+        desc:"Blades of grass flying with every kick, a quick and loud heartbeat, and an irresistible desire for triumph define both gamers and sportsmen. Rekindle your football passion and prepare your teams for the highly anticipated eFootball tournament. IHRD Tharang warmly invites each one of you to the first edition of eFootball - Road to Glory. So what are you waiting for? Coin your teams and get ready to compete and showcase your skills on the virtual pitch. ⚽",
+        link:"https://konfhub.com/road-to-glory",
+        prize:"₹5000",
+        fee:"₹25",
+        available:"Open"
     },
-    {
-        id: 3,
-        title: 'CYBOX #03',
-        img: img3,
-    },
-    {
-        id: 4,
-        title: 'CYBOX #04',
-        img: img4,
-    },
-    {
-        id: 5,
-        title: 'CYBOX #05',
-        img: img5,
-    },
-    {
-        id: 6,
-        title: 'CYBOX #06',
-        img: img6,
-    },
-    {
-        id: 7,
-        title: 'CYBOX #07',
-        img: img7,
-    },
-    {
-        id: 8,
-        title: 'CYBOX #08',
-        img: img2,
-    },
+    // {
+    //     id: 3,
+    //     title: 'COMPCOD',
+    //     img: img3,
+    //     category: '#game',
+    //     desc:"",
+    //     link:"",
+    // },
+    // {
+    //     id: 4,
+    //     title: 'CYBOX #04',
+    //     img: img4,
+    // },
+    // {
+    //     id: 5,
+    //     title: 'CYBOX #05',
+    //     img: img5,
+    // },
+    // {
+    //     id: 6,
+    //     title: 'CYBOX #06',
+    //     img: img6,
+    // },
+    // {
+    //     id: 7,
+    //     title: 'CYBOX #07',
+    //     img: img7,
+    // },
+    // {
+    //     id: 8,
+    //     title: 'CYBOX #08',
+    //     img: img2,
+    // },
 
-    {
-        id: 9,
-        title: 'CYBOX #09',
-        img: img9,
-    },
-    {
-        id: 10,
-        title: 'CYBOX #10',
-        img: img10,
-    },
-    {
-        id: 11,
-        title: 'CYBOX #11',
-        img: img11,
-    },
-    {
-        id: 12,
-        title: 'CYBOX #12',
-        img: img12,
-    },
-    {
-        id: 13,
-        title: 'CYBOX #13',
-        img: img13,
-    },
-    {
-        id: 14,
-        title: 'CYBOX #14',
-        img: img14,
-    },
-    {
-        id: 15,
-        title: 'CYBOX #15',
-        img: img1,
-    },
-    {
-        id: 16,
-        title: 'CYBOX #16',
-        img: img3,
-    },
-    {
-        id: 17,
-        title: 'CYBOX #01',
-        img: img1,
-    },
-    {
-        id: 18,
-        title: 'CYBOX #02',
-        img: img2,
-    },
-    {
-        id: 19,
-        title: 'CYBOX #03',
-        img: img3,
-    },
-    {
-        id: 20,
-        title: 'CYBOX #04',
-        img: img4,
-    },
-    {
-        id: 21,
-        title: 'CYBOX #05',
-        img: img5,
-    },
-    {
-        id: 22,
-        title: 'CYBOX #06',
-        img: img6,
-    },
-    {
-        id: 23,
-        title: 'CYBOX #07',
-        img: img7,
-    },
-    {
-        id: 24,
-        title: 'CYBOX #08',
-        img: img2,
-    },
+    // {
+    //     id: 9,
+    //     title: 'CYBOX #09',
+    //     img: img9,
+    // },
+    // {
+    //     id: 10,
+    //     title: 'CYBOX #10',
+    //     img: img10,
+    // },
+    // {
+    //     id: 11,
+    //     title: 'CYBOX #11',
+    //     img: img11,
+    // },
+    // {
+    //     id: 12,
+    //     title: 'CYBOX #12',
+    //     img: img12,
+    // },
+    // {
+    //     id: 13,
+    //     title: 'CYBOX #13',
+    //     img: img13,
+    // },
+    // {
+    //     id: 14,
+    //     title: 'CYBOX #14',
+    //     img: img14,
+    // },
+    // {
+    //     id: 15,
+    //     title: 'CYBOX #15',
+    //     img: img1,
+    // },
+    // {
+    //     id: 16,
+    //     title: 'CYBOX #16',
+    //     img: img3,
+    // },
+    // {
+    //     id: 17,
+    //     title: 'CYBOX #01',
+    //     img: img1,
+    // },
+    // {
+    //     id: 18,
+    //     title: 'CYBOX #02',
+    //     img: img2,
+    // },
+    // {
+    //     id: 19,
+    //     title: 'CYBOX #03',
+    //     img: img3,
+    // },
+    // {
+    //     id: 20,
+    //     title: 'CYBOX #04',
+    //     img: img4,
+    // },
+    // {
+    //     id: 21,
+    //     title: 'CYBOX #05',
+    //     img: img5,
+    // },
+    // {
+    //     id: 22,
+    //     title: 'CYBOX #06',
+    //     img: img6,
+    // },
+    // {
+    //     id: 23,
+    //     title: 'CYBOX #07',
+    //     img: img7,
+    // },
+    // {
+    //     id: 24,
+    //     title: 'CYBOX #08',
+    //     img: img2,
+    // },
 ]
 
 export default dataProject;
