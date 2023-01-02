f16ec97fbcb9d8812e7a5811722b999895a7e0da
Alwin <alwinjohn231@gmail.com>
2023-01-02 11:52:36 +0530

    added new talk by nabeel koya

--- src/assets/fake-data/data-blog.js
+++ src/assets/fake-data/data-blog.js
@@ -2,12 +2,22 @@ import img1 from '../images/post/career-talk.webp'
 import img2 from '../images/post/vikasanam.webp'
 import img3 from '../images/post/name-to-tame.webp'
 import img4 from '../images/post/campus.png'
-import img5 from '../images/post/volley-ball-inaugration.webp'
+import img5 from '../images/post/netizens.jpeg'
 import img6 from '../images/post/hack-league-inaugration.webp'
 
 const dataBlog = [
     {
         id: 1,
+        img : img5,
+        category: 'talk',
+        title: 'Responsible Netizens',
+        time: '02 Jan 2023',
+        text: 'Virtual talk on "Responsible Netizens" hosted by College of Engineering Chengannur, featuring Mr. Nabeel Koya A, Scientist-E at Cyber Security Group, CDAC.',
+        link:"http://min.lc/TharangTalkSeries"
+    },
+
+    {
+        id: 2,
         img : img4,
         category: 'opportunity',
         title: 'Call for CAMPUS AMBASSADORS',
@@ -16,7 +26,7 @@ const dataBlog = [
         link:"https://forms.gle/dbhBotwbjrXfjNaX6"
     },
     {
-        id: 2,
+        id: 3,
         img : img1,
         category: 'talk',
         title: 'Career Guidance',
@@ -25,7 +35,7 @@ const dataBlog = [
         link:"http://bitly.ws/yePk"
     },
     {
-        id: 3,
+        id: 4,
         img : img2,
         category: 'talk',
         title: 'Higher Education and Development',
@@ -34,7 +44,7 @@ const dataBlog = [
         link:"https://www.instagram.com/p/Cmobr9LpxOo/",
     },
     {
-        id: 4,
+        id: 5,
         img : img3,
         category: 'talk',
         title: 'Name it to Tame it',
@@ -45,7 +55,7 @@ const dataBlog = [
 
 
     {
-        id: 5,
+        id: 6,
         img : img6,
         category: 'hackathon',
         title: 'Hack-League Inauguration',
@@ -53,47 +63,13 @@ const dataBlog = [
         text: 'Inaugurated by Prof (Dr) Deepa J, Program Cordinator, IHRD Tharang Organizing committee',
     },
 
-    {
-        id: 6,
-        img : img5,
-        category: 'sports',
-        title: 'Tournament Inauguration',
-        time: '21 Dec 2022',
-        text: 'As part of IHRD Tharang, CEC warmly welcomes you to the All Kerala Volleyball Tournament',
-    },
-    
     // {
     //     id: 6,
-    //     img : img4,
-    //     category: 'talk-series',
-    //     title: 'Entrepreneurship in Kerala',
-    //     time: '20 Dec 2022',
-    //     text: 'Aren\'t you ready to take the first step toward becoming a successful entrepreneur?',
-    // },
-    
-    // {
-    //     id: 4,
-    //     img : img1,
-    //     category: 'hackathon',
-    //     title: 'Hack-League, a 24-hour Hackathon',
-    //     time: '17 Dec 2022',
-    //     text: 'College of Engineering Chengannur is here with HACK-LEAGUE, a 24-hour hackathon',
-    // },
-    // {
-    //     id: 5,
-    //     img : img2,
-    //     category: 'logo-launch',
-    //     title: 'Official Logo Launch of IHRD Tharang\'23',
-    //     time: '14 Dec 2022',
-    //     text: 'A National Techno-Cultural-Entrepreneurial Fest Organized by IHRD',
-    // },
-    // {
-    //     id: 6,
-    //     img : img3,
+    //     img : img5,
     //     category: 'sports',
-    //     title: 'Volleyball Tournament (Open to All Engr. Institutions)',
-    //     time: '13 Dec 2022',
-    //     text: 'The Volleyball tournament organized by the IHRD THARANG committee. Open to All Engr. Institutions',
+    //     title: 'Tournament Inauguration',
+    //     time: '21 Dec 2022',
+    //     text: 'As part of IHRD Tharang, CEC warmly welcomes you to the All Kerala Volleyball Tournament',
     // },
     
 ]
