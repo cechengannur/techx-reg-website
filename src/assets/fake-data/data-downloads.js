b2f30c8b533c3b274b617afe8c85cf0d83b22110
Alwin <alwinjohn231@gmail.com>
2023-01-12 04:30:45 +0530

    downloads page added

---
+++ src/assets/fake-data/data-downloads.js
@@ -0,0 +1,53 @@
+import pdf1 from '../documents/Program Chart-IHRD.pdf';
+import pdf2 from '../documents/GUIDELINES OF COLLEGE PROJECTS - THARANG.pdf';
+import pdf3 from '../documents/Guidelines for IHRD THARANG  2023 - Exhibition.pdf';
+import pdf4 from '../documents/IHRD Historical Gallery.pdf';
+import pdf5 from '../documents/Job-Fair-Guidelines.pdf';
+
+const dataDown = [
+    
+    {
+        id: 1,
+        title: 'IHRD THARANG - Complete Program Chart',
+        content: 'Complete details of the programs that will be conducted during IHRD THARANG 2023',
+        link:pdf1,
+        
+    },
+    {
+        id: 2,
+        title: 'Guidelines for IHRD THARANG 2023 - College Projects',
+        content: 'The following are the general guidelines for the applicants of IHRD THARANG - 2023',
+        link:pdf2,
+        
+    },
+    
+    {
+        id: 3,
+        title: 'General Guidelines for IHRD THARANG 2023 - Exhibition',
+        content: 'The following are the general guidelines for the applicants of IHRD THARANG - 2023',
+        link:pdf3,
+        
+    },
+    {
+        id: 4,
+        title: 'General Guidelines FOR IHRD THARANG 2023 -  IHRD Historical Gallery',
+        content: 'The following are the general guidelines for the applicants of IHRD THARANG - 2023',
+        link:pdf4,
+        
+    },
+    {
+        id: 5,
+        title: 'Job Fair Guidelines',
+        content: 'The following are the general guidelines for the Job Fair - 2023',
+        link:pdf5,
+
+    },
+    // {
+    //     id: 6,
+    //     title: 'Can we attend more than one workshops?',
+    //     content: 'Yes, you can attend as many workshops as you want.',
+        
+    // },
+]
+
+export default dataDown;
--- src/pages/DownloadsPage.jsx
+++ src/pages/DownloadsPage.jsx
@@ -1,14 +1,43 @@
 //using react router to redirect to external link
 import {React , useEffect} from 'react';
 import PageTitle from '../components/pagetitle';
-
+import dataDown from '../assets/fake-data/data-downloads';
+import AccordionItem from '../features/downloads/accordion-item';
 
 const DownloadsPage = (props) => {
-    return (
-        <div>
-          <PageTitle title='Downloads' />
+  return (
+    <div>
+        <PageTitle title='Downloads' />
+
+        <section className="tf-section faq bg-st2">
+        <div className="container">
+            <div className="row">
+                <div className="col-xl-6 col-md-12">
+                    <div className="flat-accordion" data-aos="fade-up" data-aos-duration="800">
+                        {
+                            dataDown.slice(0,3).map(item => (
+                                <AccordionItem key={item.id} item={item} />
+                                
+                            ))
+                        }
+
+                    </div>
+                </div>
+                <div className="col-xl-6 col-md-12">
+                    <div className="flat-accordion" data-aos="fade-up" data-aos-duration="800">
+                        {
+                            dataDown.slice(3,6).map(item => (
+                                <AccordionItem key={item.id} item={item} />
+                            ))
+                        }
+
+                    </div>
+                </div>
+            </div>
         </div>
-    );
+    </section>
+    </div>
+);
   }
 
 export default DownloadsPage;
